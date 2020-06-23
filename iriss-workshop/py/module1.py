a7b031d7e263a56315e35c05b31a9d3eee671b9d
Josh Grossman <jdgg@soal-2.stanford.edu>
2020-06-22 18:17:41 -0700

    updating

--- iriss-workshop/py/module1.py
+++
@@ -1,4 +0,0 @@
-# save this function to a file named module1.py
-def speak():
-    """Make module 1 say something"""
-    print('Module 1 speaking ...')
--- iriss-workshop/py/module4.py
+++
@@ -1,7 +0,0 @@
-"""Example module"""
-
-import sys
-
-if __name__ == '__main__':
-    print('The first element of sys.argv is', sys.argv[0])
-    print('The argument passed was:', sys.argv[1])
