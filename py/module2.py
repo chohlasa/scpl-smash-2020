5326ae59eb8283b34544b6c0d9cf39a80db7de0a
Alex Chohlas-Wood <chohlasa@gmail.com>
2022-01-18 12:41:28 -0800

    Removing extraneous tutorials

--- py/module2.py
+++
@@ -1,4 +0,0 @@
-# save this function to a file named module2.py
-def speak():
-    """Make module 2 say something"""
-    print('Hi, this is module 2 speaking!')
--- py/module5.py
+++
@@ -1,8 +0,0 @@
-"""Example module5"""
-
-import sys
-
-for line in sys.stdin:
-    line = line.rstrip('\n')
-    if 'two' in line.split():
-        print(line)
